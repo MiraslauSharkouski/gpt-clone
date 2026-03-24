322cce0f0c30c11d50073b6eaf73b21f2ca8ec66
Miraslau <miraslausharkouski@gmail.com>
2026-03-24 14:26:28 +0300

    add index component chat

---
+++ src/components/chat/index.ts
@@ -0,0 +1,4 @@
+export { Message } from './message';
+export { MessageList } from './message-list';
+export { ChatInput } from './chat-input';
+export { TypingIndicator, LoadingSkeleton } from './typing-indicator';
