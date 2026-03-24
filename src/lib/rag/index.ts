5af82df6d0af8421108281f418ab4ba3ae184137
Miraslau <miraslausharkouski@gmail.com>
2026-03-24 14:17:01 +0300

    add index rag lib

---
+++ src/lib/rag/index.ts
@@ -0,0 +1,4 @@
+export { extractText, countWords } from './text-extractor';
+export { chunkText, estimateTokenCount } from './chunker';
+export { generateEmbedding, generateEmbeddings } from './embeddings';
+export { searchRelevantChunks, formatContext } from './search';
